1a1243329170d6545dde439c319417357fe9c405
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-29 21:38:28 -0600

    Changed the name of the database to budgetApp.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgetApp", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
